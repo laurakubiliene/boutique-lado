6c4ffcd1a4fda5ee17d0a9d0f683982c5bdef8a6
Laura Kubiliene <lauroslapai1@gmail.com>
2021-05-11 13:34:59 +0000

    sorting

--- products/views.py
+++ products/views.py
@@ -8,7 +8,7 @@ from .models import Product, Category
 
 def all_products(request):
 
-    products = Product.object.all()
+    products = Product.objects.all()
     query = None
     categories = None
     sort = None
@@ -21,6 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+                if sortkey == 'category':
+                    sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
