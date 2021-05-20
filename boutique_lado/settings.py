44b7eba1953b9436811db0a3149a636a3e1c6235
Laura Kubiliene <lauroslapai1@gmail.com>
2021-05-20 13:44:19 +0000

    deployment part

--- boutique_lado/settings.py
+++ boutique_lado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@r*4*t0p*=evmz1f@tq-)&$7fw8qcac6oy#g*#q)xd1n*5%kce
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-lado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,14 +121,17 @@ WSGI_APPLICATION = 'boutique_lado.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
