e8e6bdc7a375ff904bbda2ff40eb54115577b493
Laura Kubiliene <lauroslapai1@gmail.com>
2021-05-20 13:57:05 +0000

    deployment

--- boutique_lado/settings.py
+++ boutique_lado/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@r*4*t0p*=evmz1f@tq-)&$7fw8qcac6oy#g*#q)xd1n*5%kce'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+
 
 ALLOWED_HOSTS = ['boutique-lado.herokuapp.com', 'localhost']
 
